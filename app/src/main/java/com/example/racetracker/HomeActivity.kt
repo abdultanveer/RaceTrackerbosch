b28f6b9a34c439bd3db03188f7504c098abd60d9
abdul <abdul.tanveer@gmail.com>
2025-03-06 12:54:47 +0530

    life after livedata

--- app/src/main/java/com/example/racetracker/HomeActivity.kt
+++ app/src/main/java/com/example/racetracker/HomeActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 
 //factory design pattern
@@ -15,6 +16,13 @@ class HomeActivity : AppCompatActivity() {
     lateinit var homeTv:TextView
     lateinit var viewModel:HomeViewModel
 
+    //made textview as an observer
+    var secsObserverphno: Observer<Int> = object : Observer<Int> {
+        override fun onChanged(seconds: Int) {
+            //receiving the update/notification
+            homeTv.setText(seconds.toString())
+        }
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +30,10 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
         viewModel = ViewModelProvider(this)[HomeViewModel::class.java]
         //this is not instantiating homeviewmodel
+
+        viewModel._seconds.observe(this, secsObserverphno);
+        //me giving my phno to the postman
+
 
 
         homeTv = findViewById(R.id.tvHome)
--- app/src/main/java/com/example/racetracker/HomeViewModel.kt
+++ app/src/main/java/com/example/racetracker/HomeViewModel.kt
@@ -2,6 +2,8 @@ package com.example.racetracker
 
 import android.os.CountDownTimer
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 //viewmodel will hold the data for ui as well as the biz logic
 //view=UI and model=data
@@ -13,7 +15,13 @@ class HomeViewModel:ViewModel() {
 
 
     var count = 0
-    var _seconds = 0
+
+    //made seconds as observable
+    //seconds is a public variable which can't be changed & _seconds is a private variable which can be channged within the class
+   //this approach of having one changeable private  variable and other unchangeable public variable is known as backing property in kotlin
+    val _seconds = MutableLiveData<Int>()//mutable = changed
+
+  //  val seconds:LiveData<Int> = _seconds.ge
 
 
     fun incrementCounter(){
@@ -29,7 +37,7 @@ class HomeViewModel:ViewModel() {
         timer = object :CountDownTimer(10_000,1_000){
 
             override fun onTick(timeRemainning: Long) {
-                _seconds = timeRemainning.toInt()
+                _seconds.value = timeRemainning.toInt()
 
                 Log.i(TAG,"seconds value ="+_seconds)
             }
